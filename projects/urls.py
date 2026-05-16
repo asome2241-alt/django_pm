60b6e0cbed096bb1cfa4b9b53ab8f270f297b7e6
Mutasım mohamed <asome2241gmail.com@Mutasms-MacBook-Air.local>
2026-05-16 14:36:08 +0300

    Refactor task workflow with toggle checkbox

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
+
     path(
         '',
         views.ProjectListView.as_view(),
@@ -11,8 +11,38 @@ urlpatterns = [
     ),
 
     path(
-        'create/',
+        'project/create/',
         views.ProjectCreateView.as_view(),
         name='project_create'
     ),
+
+    path(
+        'project/edit/<int:pk>/',
+        views.ProjectUpdateView.as_view(),
+        name='project_update'
+    ),
+
+    path(
+        'project/delete/<int:pk>/',
+        views.ProjectDeleteView.as_view(),
+        name='project_delete'
+    ),
+
+    path(
+        'task/create/<int:pk>/',
+        views.TaskCreateView.as_view(),
+        name='task_create'
+    ),
+
+
+    path(
+        'task/delete/<int:pk>/',
+        views.TaskDeleteView.as_view(),
+        name='task_delete'
+    ),
+    path(
+    'task/toggle/<int:pk>/',
+    views.task_toggle,
+    name='task_toggle'
+),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import ListView, CreateView
-from django.urls import reverse_lazy
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy,reverse
 from . import models
 from . import forms
-
+from django.shortcuts import redirect, get_object_or_404
 
 class ProjectListView(ListView):
     model = models.Project
@@ -25,3 +25,82 @@ class ProjectCreateView(CreateView):
         form.instance.user = self.request.user
 
         return super().form_valid(form)
+
+class ProjectUpdateView(UpdateView):
+
+    model = models.Project
+
+    form_class = forms.ProjectCreateForm
+
+    template_name = 'project/update.html'
+    context_object_name = 'project'
+
+    def get_success_url(self):
+
+        return reverse(
+            'project_update',
+            args=[self.object.id]
+        )
+    
+class ProjectDeleteView(DeleteView):
+
+    model = models.Project
+
+    template_name = 'project/delete.html'
+
+    success_url = reverse_lazy('project_list')
+
+class TaskCreateView(CreateView):
+
+    model = models.Task
+
+    fields = ['description']
+
+    template_name = 'project/task.html'
+
+    def form_valid(self, form):
+
+        project = models.Project.objects.get(
+            id=self.kwargs['pk']
+        )
+
+        form.instance.project = project
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+
+        return reverse(
+            'project_update',
+            args=[self.object.project.id]
+        )
+
+
+
+class TaskDeleteView(DeleteView):
+
+    model = models.Task
+
+    template_name = 'project/delete.html'
+
+    def get_success_url(self):
+
+        return reverse(
+            'project_update',
+            args=[self.object.project.id]
+        )
+def task_toggle(request, pk):
+
+    task = get_object_or_404(
+        models.Task,
+        id=pk
+    )
+
+    task.is_completed = not task.is_completed
+
+    task.save()
+
+    return redirect(
+        'project_update',
+        pk=task.project.id
+    )
